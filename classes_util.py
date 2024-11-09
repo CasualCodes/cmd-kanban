c3e4182db90c2cad5f57dd30ee1482e90c2fcb15
CasualCodes <95406523+CasualCodes@users.noreply.github.com>
2024-11-09 20:24:18 +0800

    refactor : crud handler + classes_util AND initial crud completion

--- classes_util.py
+++ classes_util.py
@@ -2,24 +2,39 @@
 # CMD - Kanban (Python)
 ###########################################################################################
 # 1. Entries
+#    1. Create
+#    2. Read
+#    3. Update
+#    4. Delete
+#    5. Move
 # 2. Columns
+#    1. Create
+#    2. Read
+#    3. Update
+#    4. Delete
+#    5. Move
 ###########################################################################################
+
+# TODO : ADD IDS
+
+#############
+## ENTRIES ##
+#############
 
 # Define Entries
 class Entry:
-    def __init__(self, name : str, content : str, column_name : str):
+    # CREATE
+    def __init__(self, name : str, content : str, column_name : str): # -> Entry
         self.name = name
         self.content = content
         self.column = column_name
 
-    # GET
-    # TODO : RECONSIDER THIS AS READ
-    def get_entry(self):
+    # GET (READ)
+    def get_entry(self): # -> Entry
         return self
     
-    # SET
-    # TODO : RECONSIDER AS WRITE
-    def set_entry(self, entry_name : str, entry_content : str, entry_column : str):
+    # SET (UPDATE)
+    def set_entry(self, entry_name : str, entry_content : str, entry_column : str) -> None:
         if (entry_name == ""):
             self.name = self.name
         else:
@@ -33,26 +48,68 @@ class Entry:
         else:
             self.column = entry_column
 
+#############
+## COLUMNS ##
+#############
 # Define Columns
 class Column:
-    def __init__(self, name : str, content : str):
+    # CREATE
+    def __init__(self, name : str, content : str): # -> Column
         self.headerTop = "="*40
         self.name = name
         self.headerBottom = "="*40
         self.content = content
         self.footer = "="*40
 
-    # GET
-    # TODO : RECONSIDER THIS AS READ
-    def get_column(self):
+    # GET (READ)
+    def get_column(self): # -> Column
         return self
 
-    # SET
-    # TODO : RECONSIDER AS WRITE    
-    def set_column(self, column_name: str):
+    # SET (UPDATE)
+    def set_column(self, column_name: str) -> None:
         if (column_name == ""):
             self.name = self.name
         else:
             self.name = column_name
     
-    
+#####################
+## LIST OPERATIONS ##
+#####################
+
+############
+##  MOVE  ##
+############
+# MOVE ENTRY
+def move_entry(container : list, entry_position : int, entry_destination : int) -> list:
+    entry : Entry = container[entry_position]
+    container.insert(entry_destination, entry)
+    container.pop(entry_position)
+    return container
+
+# MOVE COLUMN
+def move_column(container : list, column_position : int, column_destination : int) -> list:
+    # 1, 2, 3 
+    column : Column = container[column_position]
+    container.insert(column_destination, column)
+    # 1, 2, 2, 3 X
+    ## DESTINATION should be AFTER intended position ##
+    # 1, 2, 3, 2
+    container.pop(column_position)
+    # 1, 2, 3 X
+    ## DESTINATION should be AFTER intended position ##
+    # 1, 3, 2
+    return container
+
+##############
+##  DELETE  ##
+##############
+# NON URGENT TODO : POLISH
+# DELETE ENTRY
+def delete_entry(container : list, entry_position) -> list:
+    container.pop(entry_position)
+    return container
+
+# DELETE COLUMN
+def delete_column(container : list, column_position) -> list:
+    container.pop(column_position)
+    return container
--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 # CMD - Kanban (Python)
 ###########################################################################################
 import board_drawer
-import crud_handler
+# import crud_handler
 import command_handler
 import file_manager
-from classes_util import Entry
-from classes_util import Column
+from classes_util import Entry, Column, move_column, move_entry, delete_column, delete_entry
 
 # Basic Interface Display
 
@@ -21,13 +20,16 @@ def main():
 
         # TESTING # 1: INITIAL RUN 
         column_name = "Research"
-        column = crud_handler.create_column(column_name) # Create Column
+        column = Column(column_name, "")
+        # column = crud_handler.create_column(column_name) # Create Column
 
         entry_placeholders = {"name" : "value",
-                            "name2" : "value2"}
+                            "name2" : "value2",
+                            "name3" : "value3"}
         entries : Entry = []
         for key, value in entry_placeholders.items(): 
-            entries.append(crud_handler.create_entry(key, value, column_name)) # Create Entry
+            # entries.append(crud_handler.create_entry(key, value, column_name)) # Create Entry
+            entries.append(Entry(key, value, column_name))
 
         board_drawer.display_column(column, entries)
 
@@ -40,6 +42,13 @@ def main():
         # TESTING # 3: TEXT WRAP
         lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
         print(board_drawer.text_wrap(lorem))
+
+        # TESTING # 4: MOVING ENTRIES AND DELETE
+        board_drawer.display_column(column, entries)
+        entries = move_entry(entries, 1, 2)
+        board_drawer.display_column(column, entries)
+        entries = delete_entry(entries, 2)
+        board_drawer.display_column(column, entries)
 
 
 if __name__ == "__main__":
